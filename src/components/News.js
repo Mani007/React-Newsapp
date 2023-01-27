cce8a239117c0d30e218a88dfc39dd19b0ac1e3a
Mohnish <mohnish.a007@gmail.com>
2023-01-27 21:54:06 +0000

    This is for bootstrap

--- src/components/News.js
+++ src/components/News.js
@@ -1,13 +1,26 @@
-import React, { Component } from 'react'
-import NewsItem from './NewsItem'
+import React, { Component } from "react";
+import NewsItem from "./NewsItem";
 
 export default class News extends Component {
   render() {
     return (
-      <div>Latest News
-        <NewsItem/>
-        <NewsItem/>
+      <div className="container my-3">
+        <strong>Latest News</strong>
+        <div class="container">
+          <div class="row">
+            <div class="col-sm">
+            <NewsItem title="Title 1" discription="This is ONE discription" />
+            </div>
+            <div class="col-sm">
+            <NewsItem title="Title 2" discription="This is TWO discription" />
+            </div>
+            <div class="col-sm">
+            <NewsItem title="Title 3" discription="This is THREE discription" />
+            </div>
+          </div>
+        </div>
+                
       </div>
-    )
+    );
   }
 }
